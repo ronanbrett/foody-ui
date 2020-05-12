11208d4259448272fee07c3806925f4cbf481ca7
Ro Brett <ro.brett@gmail.com>
2020-05-12 01:15:41 +0100

    refactor(hooks): add typings to useObservable

--- src/hooks/useObservable.tsx
+++ src/hooks/useObservable.tsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
-const useObservable = (obs: Observable<any>) => {
-  const [state, setState] = useState({});
+const useObservable = <T extends unknown>(obs: Observable<T>) => {
+  const [state, setState] = useState<T | undefined>();
 
   useEffect(() => {
     const sub = obs.pipe(tap(x => console.log('subbed', x))).subscribe(setState);
